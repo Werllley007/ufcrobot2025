25474559c3603e86805f37408586f79e0515ef3e
Werllley007 <thiagowerlley@ufc.br>
2025-06-21 14:04:30 -0300

    etapa de ajuste dos erros do projeto

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+#from launch.substitutions import LaunchConfiguration, Command #<--- CHANGE ME
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -19,16 +20,23 @@ def generate_launch_description():
 
     package_name='ufcrobot2025' #<--- CHANGE ME
 
+    #use_ros2_control = LaunchConfiguration('use_ros2_control')#<--- CHANGE ME
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                #launch_arguments={'use_sim_time': 'true', 'use_ros2_control': use_ros2_control}.items()#<--- CHANGE ME
     )
+
+    gazebo_params_path = os.path.join(
+                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -39,10 +47,26 @@ def generate_launch_description():
 
 
 
-    # Launch them all!
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+    # Then at the bottom...
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        # ... anything else you've added in here...
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
 
